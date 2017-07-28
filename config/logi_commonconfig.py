b4fb753fe6f1c2d4ae985c3ce5ffb0f3f5b79d37
Leo Lo <llo1@logitech.com>
2017-07-28 12:48:37 +0800

    [Leo] First Upload

---
+++ config/logi_commonconfig.py
@@ -0,0 +1,9 @@
+####################################################################
+# Test script : logi_commonconfig.py                              
+# Purpose : Put all logi common config here        
+# Author : Leo Lo (llo1@logitech.com)
+# Version : v0.01
+# Date : 2017/07/28
+####################################################################
+
+### Import Packages
